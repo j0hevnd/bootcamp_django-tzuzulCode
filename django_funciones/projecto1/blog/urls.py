47b65a6b3cbac96b8330ab14c0324336130e150b
Esteban-DdS <ztheeban205@gmail.com>
2022-02-06 09:01:16 -0500

    Agregando buscador para los articulos, y algunos estilos para el HTML

--- django_funciones/projecto1/blog/urls.py
+++ django_funciones/projecto1/blog/urls.py
@@ -5,7 +5,19 @@ from .views import index, detalle_articulo, obtener_articulos_por_categorias
 app_name = "app_blog"
 
 urlpatterns = [
-    path('', index, name="inicio"),
-    path('articulo_detalle/<slug:slug>/', detalle_articulo, name="detalle"),
-    path('articulos_categoria/<str:categoria>/', obtener_articulos_por_categorias, name="categoria"),
+    path(
+        '', 
+        index, 
+        name="inicio"
+    ),
+    path(
+        'articulo_detalle/<slug:slug>/', 
+        detalle_articulo, 
+        name="detalle"
+    ),
+    path(
+        'articulos_categoria/<str:categoria>/', 
+        obtener_articulos_por_categorias, 
+        name="categoria"
+    ),
 ]
--- django_funciones/projecto1/blog/views.py
+++ django_funciones/projecto1/blog/views.py
@@ -1,28 +1,72 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
 
 # models
-from .models import Articulos
+from .models import Articulos, Categorias
 
 # Create your views here.
+
+def busqueda(request, context):
+    
+    busqueda = request.GET.get('buscar')
+    consulta = Articulos.objects.filter(
+        Q(nombre_articulo__icontains=busqueda) |
+        Q(resumen__icontains=busqueda),
+        publico = True
+    ) 
+
+    if consulta:
+        context['articulos'] = consulta
+        return context
+
+    else:
+        context['msj_no_encontrado'] = "No se encontraron coincidencias con \"%s\" " % busqueda
+    
+    return False
+
+
 def index(request):
     articulos = Articulos.objects.filter(
         publico = True
     )
-    
+
     context = {
         'articulos': articulos
     }
+    
+    if request.GET.get('buscar'):
+        busqueda(request, context)
+    
     return render(request=request, template_name="index.html", context=context)
 
 
 def obtener_articulos_por_categorias(request, categoria):
-    articulos = Articulos.objects.filter(
-        publico = True,
-        categorias__nombre_categoria = categoria
-    )
+    context = {}
     
-    return render(request, "articulos_categoria.html", {'articulos': articulos})
+    try:
+        categoria = Categorias.objects.get(nombre_categoria=categoria)
+        
+        articulos = Articulos.objects.filter(
+            publico = True,
+            categorias__nombre_categoria = categoria
+        )
+        
+        context['articulos'] = articulos
+        context['categoria'] = categoria
+        
+        if request.GET.get('buscar'):
+            busqueda(request, context)
+        
+        if articulos:
+            return render (request, "articulos_categoria.html", context)
+        
+        context['no_datos'] = "Pronto agregaremos contenido! :)"
+        
+    except Categorias.DoesNotExist:
+        context['msj_error'] = "Esta categoría no existe"
+
+    return render (request, "articulos_categoria.html", context)
 
 
 def detalle_articulo(request, slug):
@@ -34,5 +78,5 @@ def detalle_articulo(request, slug):
             return render(request, "detalle.html", context)
         
     except Articulos.DoesNotExist:
-        context['msj_error'] = "Articulo no encontrado"
+        context['msj_error'] = "Artículo no encontrado"
         return render(request, "detalle.html", context)
